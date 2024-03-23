43a71fa537254c442eaaf89466e13f3b98bd82ef
gheetdufa <gudadheer@gmail.com>
2024-03-23 19:23:52 -0400

    tutorwiz

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,77 @@
+import { Row, Col } from "antd";
+import { withTranslation } from "react-i18next";
+import { SvgIcon } from "../../common/SvgIcon";
+import Container from "../../common/Container";
+
+import i18n from "i18next";
+import {
+  FooterSection,
+  Title,
+  NavLink,
+  Extra,
+  LogoContainer,
+  Para,
+  Large,
+  Chat,
+  Empty,
+  FooterContainer,
+  Language,
+  Label,
+  LanguageSwitch,
+  LanguageSwitchContainer,
+} from "./styles";
+
+interface SocialLinkProps {
+  href: string;
+  src: string;
+}
+
+const Footer = ({ t }: any) => {
+  const handleChange = (language: string) => {
+    i18n.changeLanguage(language);
+  };
+
+  const SocialLink = ({ href, src }: SocialLinkProps) => {
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        key={src}
+        aria-label={src}
+      >
+        <SvgIcon src={src} width="25px" height="25px" />
+      </a>
+    );
+  };
+
+  return (
+    <>
+      <FooterSection>
+        <Container>
+          <Row justify="space-between">
+            <Col lg={10} md={10} sm={12} xs={12}>
+              <Language>{t("Contact")}</Language>
+              <Large to="/">{t("Please Feel Free To Reach Out!")}</Large>
+              <a href="mailto:TutorWiz@gmail.com">
+                <Chat>{t(`Email Us`)}</Chat>
+              </a>
+            </Col>
+            
+            
+          </Row>
+           
+ 
+              
+            
+          
+        </Container>
+      </FooterSection>
+      <Extra>
+        
+      </Extra>
+    </>
+  );
+};
+
+export default withTranslation()(Footer);
